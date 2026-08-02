31999c801b54f5ccbcd69de37973bbe1bd74cbcc
Karthik Venkatraman <157663259+KMmotors@users.noreply.github.com>
2026-08-02 16:35:20 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else {
+                return ispal(s, start + 1, end) || ispal(s, start, end - 1);
+            }
+        }  
+        return true;
+    }
+
+    private boolean ispal(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
